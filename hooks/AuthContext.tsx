3c34ce14d9db5ad1ffc5d9428c3010c16714ba46
Kenan Casey <kenancasey@gmail.com>
2025-10-29 14:27:11 -0500

    Add AuthContext
    Also added useAuth() hook. This was added AFTER class on Wednesday.

---
+++ hooks/AuthContext.tsx
@@ -0,0 +1,53 @@
+import { appwrite } from "@/lib/appwrite";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Models } from "react-native-appwrite";
+
+type AuthContextType = {
+    user:Models.User<Models.Preferences> | null,
+    loading: boolean,
+    login: (email:string, password:string) => Promise<void>,
+    register: (email:string, password:string, name:string) => Promise<void>
+}
+
+const AuthContext = createContext(undefined)
+
+export function AuthProvider({children}:{children:React.ReactNode}) {
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const currentUser = await appwrite.getCurrentUser();
+      setUser(currentUser)
+      setLoading(false)
+    })();
+  }, []);
+
+
+  async function login(email: string, password:string) {
+    const loggedInUser = await appwrite.loginWithEmail({email, password})
+    setUser(loggedInUser)
+  }
+
+  async function register(email:string, password:string, name:string) {
+    const loggedInUser = await appwrite.registerWithEmail({email, password,name})
+    setUser(loggedInUser)
+
+  }
+
+  return (
+    <AuthContext.Provider value={{user, loading, login, register}} >
+        {children}
+    </AuthContext.Provider>
+  )
+
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext)
+
+    if(!context) {
+        throw new Error("useAuth must be used inside <AuthProvider />")
+    }
+    return context
+}
