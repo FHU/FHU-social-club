6806256ca68a9ca40e31092d1b1420ae3b7eb31e
Kenan Casey <kenancasey@gmail.com>
2025-10-29 14:34:46 -0500

    Update AuthContext.tsx

--- hooks/AuthContext.tsx
+++ hooks/AuthContext.tsx
@@ -3,51 +3,54 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { Models } from "react-native-appwrite";
 
 type AuthContextType = {
-    user:Models.User<Models.Preferences> | null,
-    loading: boolean,
-    login: (email:string, password:string) => Promise<void>,
-    register: (email:string, password:string, name:string) => Promise<void>
+  user: Models.User<Models.Preferences> | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string, name: string) => Promise<void>;
 }
 
-const AuthContext = createContext(undefined)
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export function AuthProvider({children}:{children:React.ReactNode}) {
-  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null
+  );
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const currentUser = await appwrite.getCurrentUser();
-      setUser(currentUser)
-      setLoading(false)
+      setUser(currentUser);
+      setLoading(false);
     })();
   }, []);
 
-
-  async function login(email: string, password:string) {
-    const loggedInUser = await appwrite.loginWithEmail({email, password})
-    setUser(loggedInUser)
+  async function login(email: string, password: string) {
+    const loggedInUser = await appwrite.loginWithEmail({ email, password });
+    setUser(loggedInUser);
   }
 
-  async function register(email:string, password:string, name:string) {
-    const loggedInUser = await appwrite.registerWithEmail({email, password,name})
-    setUser(loggedInUser)
-
+  async function register(email: string, password: string, name: string) {
+    const loggedInUser = await appwrite.registerWithEmail({
+      email,
+      password,
+      name,
+    });
+    setUser(loggedInUser);
   }
 
   return (
-    <AuthContext.Provider value={{user, loading, login, register}} >
-        {children}
+    <AuthContext.Provider value={{ user, loading, login, register }}>
+      {children}
     </AuthContext.Provider>
-  )
-
+  );
 }
 
 export function useAuth() {
-    const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
-    if(!context) {
-        throw new Error("useAuth must be used inside <AuthProvider />")
-    }
-    return context
+  if (!context) {
+    throw new Error("useAuth() must be used inside <AuthProvider />");
+  }
+  return context;
 }
